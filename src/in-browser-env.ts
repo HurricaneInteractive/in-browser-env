7fe185359a4de79d94720cd7bd3b5156aad7d0ab
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-13 09:15:43 +1100

    feat(added functionality): Added inBrowserEnv method and tests

--- src/in-browser-env.ts
+++ src/in-browser-env.ts
@@ -1,6 +1,10 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
-
+/**
+ * Checks if the current environment is the browser
+ *
+ * @return {boolean}
+ */
+const inBrowserEnv = (): boolean => {
+  return typeof window !== 'undefined' && typeof document !== 'undefined'
 }
+
+export default inBrowserEnv
--- test/in-browser-env.test.ts
+++ test/in-browser-env.test.ts
@@ -1,14 +1,12 @@
-import DummyClass from "../src/in-browser-env"
+import inBrowserEnv from '../src/in-browser-env'
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+describe('inBrowserEnv Test', () => {
+  it('Is defined', () => {
+    expect(inBrowserEnv).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('Is not a browser env', () => {
+    const isBrowser = inBrowserEnv()
+    expect(isBrowser).toBeFalsy()
   })
 })
